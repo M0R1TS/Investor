95791a04842c90f30294b2ebe0cf3fbd270b1dd2
morits <mor1ts@mail.ru>
2024-05-31 19:09:48 +0300

    Добавлена кнопка паузы, курс переведён в доллары

--- app/src/main/java/ru/devsokovix/investor/MainActivity.kt
+++ app/src/main/java/ru/devsokovix/investor/MainActivity.kt
@@ -8,17 +8,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableBoolean
 import ru.devsokovix.investor.databinding.ActivityMainBinding
 import kotlin.random.Random
 
+fun getExRate(): Double {
+    return 90.432
+}
+
 class MainActivity : AppCompatActivity() {
     lateinit var countDownTimer: CountDownTimer
+    var flag: ObservableBoolean = ObservableBoolean(true)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val user = User(50_000, arrayListOf(Stock("Яндекс", 5000, R.drawable.iconyandex), Stock("Mail.ru Group",2000, R.drawable.iconmail)))
         binding.user = user
+        binding.exRate = getExRate()
 
+        binding.f = {
+            flag.set(!flag.get())
+            if (flag.get()) {
+                countDownTimer.start()
+            }else {
+                countDownTimer.cancel()
+            }
+        }
+        binding.flag = flag
         (binding.recyclerView.adapter) =  StockAdapter(user.stockList)
         val r = Random(System.currentTimeMillis())
         countDownTimer = object : CountDownTimer(10_000, 1000L){
